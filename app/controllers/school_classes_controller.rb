20df46d6ed512ce248a14a85506a712cbaf9af18
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-01-09 13:26:02 -0800

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,36 @@
+require 'pry'
+class SchoolClassesController < ApplicationController
+    def index
+        @school_classes = SchoolClass.all
+    end
+
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def new
+        @school_class = SchoolClass.all
+    end
+
+    def create
+        @school_class = SchoolClass.new(post_params(:title, :room_number))
+        @school_class.save
+        redirect_to schoolclass_path(@school_class)
+      end
+    
+    def edit
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(post_params(:title))
+        @school_class.update(post_params(:room_number))
+        redirect_to schoolclass_path(@school_class)
+      end
+
+    private
+      def post_params(*args)
+        params.require(:schoolclass).permit(*args)
+      end
+end
